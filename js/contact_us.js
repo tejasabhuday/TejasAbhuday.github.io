9f1ce01791fb77315007d4d30a33f0b79fe375ea
Tejas <tejasabuday@gmail.com>
2023-12-20 21:49:09 +0530

    Fifth

--- js/contact_us.js
+++ js/contact_us.js
@@ -30,7 +30,7 @@ $(".contact_btn").on('click', function () {
                 handleResponse(response);
             },
             error: function () {
-                handleResponse({ type: 'error', text: 'Thankyou for contacting i will make sure to get back to you as soon as possible i am very active on LinkedIn so you can directly contact me there..' });
+                handleResponse({ type: 'error', text: 'Thankyou for contacting i will make sure to get back to you as soon as possible i am very active on LinkedIn so you can directly contact me there.' });
             }
         });
         
